492a551078a9bc5baa50cf4e2b957eeda2d54161
PokerFace777 <andrewjeremy12345@gmail.com>
2019-09-27 23:30:45 +0700

    pengumuman final

--- dist/print.cpp
+++ dist/print.cpp
@@ -4,14 +4,16 @@ using namespace std;
 string p1[N],p2[N],p3[N],school[N];
 
 int main() {
-    for (int i = 1 ; i <= 37 ; i++)
+    int n; scanf("%d" , &n);
+    string nld; getline(cin , nld);
+    for (int i = 1 ; i <= n ; i++)
         getline(cin , school[i]);
-    for (int i = 1 ; i <= 37 ; i++)
+    for (int i = 1 ; i <= n ; i++)
         getline(cin , p1[i]);
-    for (int i = 1 ; i <= 37 ; i++)
+    for (int i = 1 ; i <= n ; i++)
         getline(cin , p2[i]);
-    for (int i = 1 ; i <= 37 ; i++)
+    for (int i = 1 ; i <= n ; i++)
         getline(cin , p3[i]);
-    for (int i = 1 ; i <= 37 ; i++)
+    for (int i = 1 ; i <= n ; i++)
         printf("<tr>\n    <th>%d.</th>\n    <th>%s</th>\n    <th>%s</th>\n    <th>%s</th>\n    <th>%s</th>\n</tr>\n" , i , school[i].c_str() , p1[i].c_str() , p2[i].c_str() , p3[i].c_str());
 }
