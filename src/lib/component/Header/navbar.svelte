240a4b3524458d3a265e6ec6df8399946e1a6f7c
plty <platyunyu@gmail.com>
2021-08-06 19:07:34 +0000

    scrolly-doo

--- src/lib/component/Header/navbar.svelte
+++ src/lib/component/Header/navbar.svelte
@@ -1,43 +1,59 @@
 <script>
-	import logo from './wide-logo-small.png';
+	import logo from "./wide-logo-small.png";
 	export let path;
+	import { scrollTo } from "svelte-scrolling";
+	import { setGlobalOptions } from "svelte-scrolling";
+
+	setGlobalOptions({
+		offset: -80,
+	});
 </script>
 
 <div class="flex items-center">
 	<div class="flex-shrink-0">
-		<img
-			class="h-12 w-auto"
-			alt="bcomp"
-			src={logo}
-		/>
+		<img class="h-12 w-auto" alt="bcomp" src={logo} />
 	</div>
 	<div class="hidden md:block">
 		<div class="ml-10 flex items-baseline space-x-4">
 			<a
 				sveltekit:prefetch
-				href="/"
+				use:scrollTo={"top-index"}
 				class="px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white;"
-				class:nav-active={path === '/'}
 			>
 				Home
 			</a>
 			<a
 				sveltekit:prefetch
-				href="/about"
+				use:scrollTo={"prizes"}
 				class="px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white;"
-				class:nav-active={path === '/about'}
 			>
-				About
+				Prizes
 			</a>
 			<a
 				sveltekit:prefetch
-				href="/about"
-				class="px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white;" class:nav-active={path === '/about'}
+				use:scrollTo={"schedule"}
+				class="px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white;"
 			>
-				Prizes
-			</a>		</div>
+				Schedule
+			</a>
+			<a
+				sveltekit:prefetch
+				use:scrollTo={"howto"}
+				class="px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white;"
+			>
+				How To
+			</a>
+			<a
+				sveltekit:prefetch
+				use:scrollTo={"about-us"}
+				class="px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white;"
+			>
+				About Us
+			</a>
+		</div>
 	</div>
 </div>
+
 <style lang="postcss">
 	.nav-active {
 		@apply bg-gray-900 text-white;
