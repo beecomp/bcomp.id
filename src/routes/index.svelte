1a7d2ab86c2467503d29bbcee9b8883b4f017861
plty <platyunyu@gmail.com>
2021-08-07 04:20:27 +0000

    add detailed schedule

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { slide } from "svelte/transition";
 	import Icon from "fa-svelte";
 	import {
 		faTrophy,
@@ -7,6 +8,7 @@
 		faBullhorn,
 		faPencilAlt,
 		faChevronDown,
+		faChevronUp,
 	} from "@fortawesome/free-solid-svg-icons";
 	import { faWhatsapp, faLine, faInstagram } from "@fortawesome/free-brands-svg-icons";
 	import bca from "./bca.svg";
@@ -19,6 +21,61 @@
 			window.location.href = url;
 		};
 	}
+	let showDetailedSchedule = false;
+	const detailedSchedule = [
+		{
+			date: "Senin, 6 Sept 2021",
+			events: [
+				{
+					time: "08:00–09:00",
+					event: "Pembukaan",
+				},
+				{
+					time: "09:30–10:30",
+					event: "Penyisihan\u00a02",
+				},
+				{
+					time: "17:00",
+					event: "Pengumuman Penyisihan\u00a02",
+				},
+			],
+		},
+		{
+			date: "Selasa, 7 Sept 2021",
+			events: [
+				{
+					time: "09:00–12:00",
+					event: "Semifinal Matematika",
+				},
+				{
+					time: "13:00–15:00",
+					event: "Semifinal IPA",
+				},
+			],
+		},
+		{
+			date: "Kamis, 9 Sept 2021",
+			events: [
+				{
+					time: "17:00",
+					event: "Pengumuman Semifinal",
+				},
+			],
+		},
+		{
+			date: "Jumat, 10 Sept 2021",
+			events: [
+				{
+					time: "8:30–12:00",
+					event: "Final",
+				},
+				{
+					time: "13:30–14:00",
+					event: "Pengumuman Pemenang",
+				},
+			],
+		},
+	];
 	const schedule = [
 		{
 			icon: faInbox,
@@ -78,7 +135,7 @@
 			<h1 class="text-center text-4xl font-black mt-6 mb-3">Prizes</h1>
 			<div class="flex flex-col items-center sm:items-start sm:flex-row justify-center">
 				<div class="hidden md:block flex-grow mx-1">
-					<div class="simple-card shadow-lg py-4 ib-gradient opacity-80">
+					<div class="rounded-xl shadow-lg py-4 ib-gradient opacity-80">
 						<div class="flex flex-col items-center">
 							<Icon icon={faAward} class="text-6xl mt-1 text-white" />
 							<div class="text-white font-bold mt-1">Juara 4</div>
@@ -87,7 +144,7 @@
 					</div>
 				</div>
 				<div class="hidden sm:block flex-grow mx-1">
-					<div class="simple-card shadow-xl py-4 ib-gradient mt-6 opacity-90">
+					<div class="rounded-xl shadow-xl py-4 ib-gradient mt-6 opacity-90">
 						<div class="flex flex-col items-center">
 							<Icon icon={faAward} class="text-7xl mt-3 text-white" />
 							<div class="text-white font-bold text-xl mt-2">Juara 2</div>
@@ -96,7 +153,7 @@
 					</div>
 				</div>
 				<div class="flex-grow mx-1 max-w-xs">
-					<div class="simple-card shadow-2xl py-4 ib-gradient md:mt-8 px-6 ">
+					<div class="rounded-xl shadow-2xl py-4 ib-gradient md:mt-8 px-6 ">
 						<div class="flex flex-col items-center">
 							<Icon icon={faTrophy} class="text-9xl mt-4 text-white" />
 							<div class="text-white font-bold text-2xl mt-3">Champion</div>
@@ -108,7 +165,7 @@
 				</div>
 
 				<div class="hidden sm:block flex-grow mx-1">
-					<div class="simple-card shadow-xl py-4 ib-gradient mt-6 opacity-90">
+					<div class="rounded-xl shadow-xl py-4 ib-gradient mt-6 opacity-90">
 						<div class="flex flex-col items-center">
 							<Icon icon={faAward} class="text-7xl mt-3 text-white" />
 							<div class="text-white font-bold text-xl mt-2">Juara 3</div>
@@ -118,7 +175,7 @@
 				</div>
 
 				<div class="hidden md:block flex-grow mx-1">
-					<div class="simple-card shadow-lg py-4 ib-gradient opacity-80">
+					<div class="rounded-xl shadow-lg py-4 ib-gradient opacity-80">
 						<div class="flex flex-col items-center">
 							<Icon icon={faAward} class="text-6xl mt-1 text-white" />
 							<div class="text-white font-bold mt-1">Juara 5</div>
@@ -128,7 +185,7 @@
 				</div>
 			</div>
 			<div class="flex flex-col items-center mt-2 space-y-2">
-				<div class="sm:hidden simple-card py-2 ib-gradient w-56">
+				<div class="sm:hidden rounded-xl py-2 ib-gradient w-56">
 					<div class="flex flex-row items-center justify-between px-4">
 						<Icon icon={faAward} class="text-4xl mt-1 text-white" />
 						<div class="flex flex-col items-center">
@@ -137,7 +194,7 @@
 						</div>
 					</div>
 				</div>
-				<div class="sm:hidden simple-card py-2 ib-gradient w-56">
+				<div class="sm:hidden rounded-xl py-2 ib-gradient w-56">
 					<div class="flex flex-row items-center justify-between px-4">
 						<Icon icon={faAward} class="text-4xl mt-1 text-white" />
 						<div class="flex flex-col items-center">
@@ -146,7 +203,7 @@
 						</div>
 					</div>
 				</div>
-				<div class="md:hidden simple-card py-2 ib-gradient w-56">
+				<div class="md:hidden rounded-xl py-2 ib-gradient w-56">
 					<div class="flex flex-row items-center justify-between px-4">
 						<Icon icon={faAward} class="text-4xl mt-1 text-white" />
 						<div class="flex flex-col items-center">
@@ -155,7 +212,7 @@
 						</div>
 					</div>
 				</div>
-				<div class="md:hidden simple-card py-2 ib-gradient w-56">
+				<div class="md:hidden rounded-xl py-2 ib-gradient w-56">
 					<div class="flex flex-row items-center justify-between px-4">
 						<Icon icon={faAward} class="text-4xl mt-1 text-white" />
 						<div class="flex flex-col items-center">
@@ -168,12 +225,12 @@
 		</div>
 
 		<div use:scrollRef={"schedule"}>
-			<h1 class="text-center text-5xl font-black my-6">Schedule</h1>
+			<h1 class="text-center text-4xl font-black my-6">Schedule</h1>
 			<div
 				class="flex flex-col md:flex-row items-center md:items-stretch md:space-x-3 space-y-2 md:space-y-0"
 			>
 				{#each schedule as { icon, event, date }}
-					<div class="ib-gradient simple-card p-2 max-w-sm w-full">
+					<div class="ib-gradient rounded-xl p-2 max-w-sm w-full filter shadow-lg">
 						<div class="flex flex-row md:flex-col items-center">
 							<Icon {icon} class="text-5xl mx-8 md:mt-2 text-white" />
 							<div class="flex flex-col md:items-center md:mt-2">
@@ -184,38 +241,56 @@
 					</div>
 				{/each}
 			</div>
-			<div class="flex flex-row justify-center mt-3">
+			<div class="flex flex-col items-center mt-1">
+				{#if showDetailedSchedule}
+					<div transition:slide={{ duration: 500 }}>
+						<table>
+							{#each detailedSchedule as { date, events }}
+								<tr>
+									<th class="pt-3 border-b-2 border-gray-500" colspan="2">{date}</th>
+								</tr>
+								{#each events as { time, event }}
+									<tr class>
+										<td class="text-right">{time}</td>
+										<td class="px-2">{event}</td>
+									</tr>
+								{/each}
+							{/each}
+						</table>
+					</div>
+				{/if}
 				<button
 					type="button"
-					class="max-w-xs py-2 px-4 bg-yellow-500 hover:bg-yellow-600 focus:ring-yellow-600 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg "
+					class="mt-3 max-w-md py-2 px-4 bg-yellow-500 hover:bg-yellow-600 focus:ring-yellow-600 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg "
+					on:click={() => (showDetailedSchedule = !showDetailedSchedule)}
 				>
-					<Icon icon={faChevronDown} class="mx-4 text-white" />
-					Selengkapnya
-					<Icon icon={faChevronDown} class="mx-4 text-white" />
+					<Icon icon={showDetailedSchedule ? faChevronUp : faChevronDown} class="mx-4 text-white" />
+					{showDetailedSchedule ? "Sembunyikan" : "Selengkapnya"}
+					<Icon icon={showDetailedSchedule ? faChevronUp : faChevronDown} class="mx-4 text-white" />
 				</button>
 			</div>
 		</div>
 		<div use:scrollRef={"howto"}>
-			<h1 class="text-center text-5xl font-black my-6">How to participate</h1>
+			<h1 class="text-center text-4xl font-black my-6">How to participate</h1>
 
-			<h2 class="text-center text-3xl font-black mt-6 mb-2">Baca Guidebook</h2>
+			<h2 class="text-center text-2xl font-black mt-6 mb-2">Baca Guidebook</h2>
 			<div class="flex flex-row justify-center space-x-4">
 				<button
 					type="button"
-					class="max-w-xs py-2 px-4 bg-yellow-500 hover:bg-yellow-600 focus:ring-yellow-600 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg "
+					class="w-48 py-2 px-4 bg-yellow-500 hover:bg-yellow-600 focus:ring-yellow-600 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg "
 					on:click={href("https://www.google.com")}
 				>
 					Matematika
 				</button>
 				<button
 					type="button"
-					class="max-w-xs py-2 px-4 bg-yellow-500 hover:bg-yellow-600 focus:ring-yellow-600 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg "
+					class="w-48 py-2 px-4 bg-yellow-500 hover:bg-yellow-600 focus:ring-yellow-600 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg "
 				>
 					IPA
 				</button>
 			</div>
 
-			<h2 class="text-center text-3xl font-black mt-6">Bayar Rp 100.000,- per tim</h2>
+			<h2 class="text-center text-2xl font-black mt-6">Bayar Rp 100.000,- per tim</h2>
 			<div class="flex flex-row justify-center mt-2">
 				<div class="w-72 shadow-lg rounded-2xl bg-white dark:bg-gray-800 p-4">
 					<div class="flex-row gap-4 flex justify-center items-center">
@@ -233,48 +308,48 @@
 				</div>
 			</div>
 
-			<h2 class="text-center text-3xl font-black mt-6">Isi form pendaftaran</h2>
+			<h2 class="text-center text-2xl font-black mt-6">Isi form pendaftaran</h2>
 			<div class="flex flex-row justify-center mt-2">
 				<button
 					type="button"
-					class="max-w-xs py-2 px-4 bg-yellow-500 hover:bg-yellow-600 focus:ring-yellow-600 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg "
+					class="w-64 py-2 px-4 bg-yellow-500 hover:bg-yellow-600 focus:ring-yellow-600 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg "
 				>
 					<span class="text-2xl">Isi Form</span>
 				</button>
 			</div>
 
-			<h2 class="text-center text-3xl font-black mt-6 mb-2">Kerjakan Soal</h2>
+			<h2 class="text-center text-2xl font-black mt-6 mb-2">Kerjakan Soal</h2>
 			<div class="flex flex-row justify-center space-x-4">
 				<button
 					type="button"
-					class="max-w-xs py-2 px-4 bg-yellow-500 hover:bg-yellow-600 focus:ring-yellow-600 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg "
+					class="w-48 py-2 px-4 bg-yellow-500 hover:bg-yellow-600 focus:ring-yellow-600 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg "
 				>
 					Matematika
 				</button>
 				<button
 					type="button"
-					class="max-w-xs py-2 px-4 bg-yellow-500 hover:bg-yellow-600 focus:ring-yellow-600 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg "
+					class="w-48 py-2 px-4 bg-yellow-500 hover:bg-yellow-600 focus:ring-yellow-600 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg "
 				>
 					IPA
 				</button>
 			</div>
-			<h2 class="text-center text-3xl font-black mt-6">Kumpulkan Jawaban Sebelum 1 September</h2>
+			<h2 class="text-center text-2xl font-black mt-6">Kumpulkan Jawaban Sebelum 1 September</h2>
 			<div class="flex flex-row justify-center mt-2">
 				<button
 					type="button"
-					class="max-w-xs py-2 px-4 bg-yellow-500 hover:bg-yellow-600 focus:ring-yellow-600 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg "
+					class="w-64 py-2 px-4 bg-yellow-500 hover:bg-yellow-600 focus:ring-yellow-600 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg "
 				>
 					<span class="text-2xl">Kumpulkan</span>
 				</button>
 			</div>
-			<h2 class="text-center text-3xl font-black mt-6">Tunggu Pengumuman</h2>
+			<h2 class="text-center text-2xl font-black mt-6">Tunggu Pengumuman Tanggal 3 September</h2>
 			<div class="max-w-3xl mx-auto mt-2">
-				80% dari peserta dan sebanyak-banyak nya 50 orang dari setiap bidang akan diloloskan ke maju
-				ke tahap selanjutnya
+				80% dari peserta dan sebanyak-banyak nya 50 orang dari setiap bidang akan lolos maju ke
+				tahap selanjutnya
 			</div>
 		</div>
 		<div use:scrollRef={"about-us"}>
-			<h1 class="text-center text-5xl font-black my-6">About Us</h1>
+			<h1 class="text-center text-4xl font-black my-6">About Us</h1>
 			<div class="shadow-lg rounded-xl bg-white p-8 pt-4">
 				<p class="text-justify">
 					Pls tulis ulang yang sebelomnya nulis bikinnya kyk orang stroke. mkz. Bacon ipsum dolor
@@ -299,7 +374,7 @@
 >
 	<div>
 		<span class="text-2xl font-bold">Contact</span>
-		<ul class="mt-1 space-y-2">
+		<ul class="mt-1 space-y-1">
 			<li>
 				<a
 					href="https://wa.me/6281333047778?text=Hi Elva! Saya ingin bertanya tentang Brilliant Competition XIII"
@@ -344,8 +419,5 @@
 <style lang="postcss">
 	.ib-gradient {
 		@apply bg-gradient-to-br from-yellow-600 to-yellow-500;
-	}
-	.simple-card {
-		@apply rounded-xl;
 	}
 </style>
