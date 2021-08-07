465592a5f1052660aea973bf491df35af53015b7
erickj04 <88482247+erickj04@users.noreply.github.com>
2021-08-07 18:21:32 +0700

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -314,7 +314,7 @@
 					class="w-48 py-2 px-4 bg-yellow-500 hover:bg-yellow-600 focus:ring-yellow-600 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg "
 					on:click={href("https://drive.google.com/drive/folders/1PVTifCESSpERpZNFK2bNBvOKU71sOdoS")}
 				>
-					Download
+					Download Guidebook
 				</button>
 			</div>
 
