02f804bb614e8b00565d24117be22fee0a5bf4c9
plty <platyunyu@gmail.com>
2021-08-08 10:49:35 +0000

    refactor code

---
+++ src/lib/component/Faq/index.svelte
@@ -0,0 +1,16 @@
+<script>
+	import FaqBox from "./FaqBox.svelte";
+	export let qnas;
+</script>
+
+<h1 class="font-montserrat italic text-center text-5xl text-gray-700">FAQ</h1>
+<div class="flex flex-col space-y-2 max-w-4xl mt-3 mx-auto">
+	{#each qnas as { q, a }}
+		<FaqBox {q} {a} />
+	{/each}
+	<span class="font-bold text-center">
+		Tidak bisa menemukan pertanyaanmu?
+		<br />
+		Tanya langsung lewat widget di kanan bawah layar atau kontak yang tersedia.
+	</span>
+</div>
---
+++ src/lib/component/HowToJoin/index.svelte
@@ -0,0 +1,113 @@
+<script type="text/javascript">
+	import bca from "./bca.svg";
+</script>
+
+<h1 class="font-montserrat italic text-center text-5xl text-gray-700">HOW TO JOIN</h1>
+<h2 class="font-montserrat text-center text-2xl font-black mt-3 mb-2">Baca Guidebook</h2>
+<div class="flex flex-row justify-center space-x-4">
+	<a
+		target="_blank"
+		rel="noreferrer"
+		href="https://drive.google.com/file/d/10GRm2eks2lfOsvNUaNJfqkhg_0nZxn8b/view"
+		class="btn w-48"
+	>
+		Guidebook
+	</a>
+</div>
+
+<h2 class="font-montserrat text-center text-2xl font-black mt-6">
+	Bayar <span class="thiccboi line-through text-gray-500">Rp&nbsp;150.000</span> Rp&nbsp;100.000,- per
+	tim
+</h2>
+<div class="font-montserrat text-center  text-gray-900">Harga spesial s.d. 19 Agustus 2019</div>
+<div class="flex flex-row justify-center mt-2">
+	<div class="w-72 shadow-lg rounded bg-white dark:bg-gray-800 p-4">
+		<div class="flex-row gap-4 flex justify-center items-center">
+			<div class="flex-shrink-0">
+				<img alt="BCA" src={bca} class="mx-auto object-cover rounded-full h-16 w-16 " />
+			</div>
+			<div class=" flex flex-col">
+				<span class="font-barlow font-semibold text-gray-800 dark:text-white text-lg"
+					>6050 5757 97</span
+				>
+				<span class="font-barlow text-gray-700 text-xs"> Lucia Helda Sofyananigrum </span>
+				<span class="font-barlow text-gray-700 text-xs"> Dimas Ambrosius Kridhananda </span>
+			</div>
+		</div>
+	</div>
+</div>
+
+<h2 class="font-montserrat text-center text-2xl mt-6">Isi form pendaftaran</h2>
+<div class="flex flex-row justify-center mt-2">
+	<a
+		target="_blank"
+		rel="noreferrer"
+		href="https://docs.google.com/forms/d/e/1FAIpQLSfcbC-AjV7EHEh3PuyCWZPm_9ZOLvhLQlMi_4ocHeUky9wqpw/viewform"
+		class="btn w-64"
+	>
+		Kerjakan Form
+	</a>
+</div>
+
+<h2 class="font-montserrat text-center text-2xl mt-6 mb-2">Kerjakan Soal</h2>
+<div class="flex flex-row justify-center space-x-4">
+	<a
+		target="_blank"
+		rel="noreferrer"
+		href="https://drive.google.com/file/d/1PuC-VFzFnyR0gHyKNrXs3fNSSWTAuVZa/view"
+		class="btn w-40"
+	>
+		Soal MAT
+	</a>
+	<a
+		target="_blank"
+		rel="noreferrer"
+		href="https://drive.google.com/file/d/1Bh883igKqC0qVdCssnT69AdgNMlvyYKm/view"
+		class="btn w-40"
+	>
+		Soal IPA
+	</a>
+</div>
+<div class="flex flex-row justify-center space-x-4 mt-2">
+	<a
+		target="_blank"
+		rel="noreferrer"
+		href="https://drive.google.com/file/d/1xNjNc4sgMWXRk_T5rOeZbeWb5pGAxUqg/view"
+		class="btn w-40"
+	>
+		LJ MAT
+	</a>
+	<a
+		target="_blank"
+		rel="noreferrer"
+		href="https://drive.google.com/file/d/1BlLUZS4869F_KjgzGHHUODjyXuLQJKi3/view"
+		class="btn w-40"
+	>
+		LJ IPA
+	</a>
+</div>
+<h2 class="font-montserrat text-center text-2xl mt-6">Kumpulkan Jawaban Sebelum 1 September</h2>
+<div class="flex flex-row justify-center mt-2">
+	<a
+		target="_blank"
+		rel="noreferrer"
+		href="https://docs.google.com/forms/d/e/1FAIpQLScs8F5TeQdbZImYiSKeU18wKVVTmxkW8lJ2pHPqw-_IYU85vw/viewform"
+		class="btn w-64"
+	>
+		Kumpulkan
+	</a>
+</div>
+<h2 class="font-montserrat text-center text-2xl mt-6">Tunggu Pengumuman Tanggal 4 September</h2>
+<div class="max-w-3xl mx-auto mt-2">
+	80% dari peserta dan sebanyak-banyak nya 50 tim dari setiap bidang akan lolos maju ke tahap
+	selanjutnya
+</div>
+
+<style lang="postcss">
+	.btn {
+		@apply font-barlow font-medium py-2 px-4 bg-coral hover:bg-yellow-600 focus:ring-yellow-600 focus:ring-offset-yellow-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded;
+	}
+	.thiccboi {
+		text-decoration-thickness: 15%;
+	}
+</style>
