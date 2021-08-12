d13be6c1c524d1620090cf5e1c07bba57fb3231b
jlebang07 <j.lebang07@gmail.com>
2021-08-12 21:11:13 +0700

    Change guidebook link

--- src/lib/component/HowToJoin/index.svelte
+++ src/lib/component/HowToJoin/index.svelte
@@ -8,7 +8,7 @@
 	<a
 		target="_blank"
 		rel="noreferrer"
-		href="https://drive.google.com/file/d/1MgHI-7kz8hdmGo9FOsbfLUaeE1vja5Vv/view?usp=sharing"
+    href="https://drive.google.com/file/d/1MgHI-7kz8hdmGo9FOsbfLUaeE1vja5Vv/view?usp=sharing"
 		class="btn w-64"
 	>
 		Guidebook
