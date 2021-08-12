3a28a0fc956637416dda11318454314a27226f1f
jer <jer@moo>
2021-08-12 06:04:16 +0000

    improve SEO

---
+++ src/routes/register.svelte
@@ -0,0 +1,13 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<script>
+	import { onMount } from "svelte";
+	import { goto } from "$app/navigation";
+	onMount(async () => {
+		goto(
+			"https://docs.google.com/forms/d/e/1FAIpQLSfcbC-AjV7EHEh3PuyCWZPm_9ZOLvhLQlMi_4ocHeUky9wqpw/viewform",
+		);
+	});
+</script>
---
+++ src/routes/soal-ipa.svelte
@@ -0,0 +1,11 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<script>
+	import { onMount } from "svelte";
+	import { goto } from "$app/navigation";
+	onMount(async () => {
+		goto("https://drive.google.com/file/d/1Bh883igKqC0qVdCssnT69AdgNMlvyYKm/view");
+	});
+</script>
