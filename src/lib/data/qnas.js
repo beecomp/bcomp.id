fe8a1775b340abc4b492646ffd70f99dd10f5f92
Nicholas Ryan PH <nicholasryanph@gmail.com>
2022-08-18 21:09:26 +0700

    tambah qnans

--- src/lib/data/qnas.js
+++ src/lib/data/qnas.js
@@ -12,6 +12,10 @@ const s = [
 		a: "Early (20 Agustus - 31 Agustus 2022) : Rp 125.000,00 - per tim, Regular (1 September - 17 September 2022) : Rp 150.000,00 - per tim",
 	},
 	{
+		q: "Dimana bisa akses soal P1?",
+		a: "Daftar dan bayar dulu, submit ke google form. Baru email yang berisi file soal (untuk bidang IPA) atau link google form soal (untuk bidang mat) akan dikirimkan ke email peserta 1 yang sudah didaftarkan"
+	},
+	{
 		q: "Berapa banyak tim yang akan lolos dari penyisihan 2 ke semifinal?",
 		a: "16 tim",
 	},
