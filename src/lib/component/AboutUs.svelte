8aa66a65b26fe9d28f0be26af1db75cd982a6bab
NicholasRyan <88495133+NicholasRyan@users.noreply.github.com>
2022-08-18 09:37:10 +0700

    Update AboutUs.svelte

--- src/lib/component/AboutUs.svelte
+++ src/lib/component/AboutUs.svelte
@@ -2,13 +2,13 @@
 <div class="mt-3 max-w-4xl mx-auto shadow-lg rounded-xl bg-white px-8 py-4">
 	<h4 class="font-barlow font-bold text-xl">Brilliant Competition</h4>
 	<p class="text-justify mt-1">
-		Brilliant Competition XIII adalah kegiatan kompetisi olimpiade untuk bidang matematika dan sains
+		Brilliant Competition XIV adalah kegiatan kompetisi olimpiade untuk bidang matematika dan sains
 		tingkat SMP berskala nasional yang akan diselenggarakan untuk ketiga belas kalinya secara online
-		di SMAK PENABUR Gading Serpong. Brilliant Competition XII sebelumnya diikuti oleh ratusan
+		di SMAK PENABUR Gading Serpong. Brilliant Competition XIII sebelumnya diikuti oleh ratusan
 		peserta dari sembilan provinsi dan lebih dari 60 sekolah yang tersebar di Indonesia. Brilliant
 		Competition dirancang dan dilaksanakan oleh seluruh siswa Brilliant Class di SMAK Penabur Gading
-		Serpong beserta dengan guru-guru terpilih dari Brilliant Class. Tema Brilliant Competition XIII
-		tahun ini adalah “Unlocking Your Brilliance”. Pada Brilliant Competition XIII, kami mengajak
+		Serpong beserta dengan guru-guru terpilih dari Brilliant Class. Tema Brilliant Competition XIV
+		tahun ini adalah “A New Age of Brilliance”. Pada Brilliant Competition XIV, kami mengajak
 		siswa-siswi yang berpotensi dan unggul untuk membuka pikiran mereka dan menunjukkan potensi
 		kecerdasan mereka masing-masing. Melalui kompetisi ini, kami berharap agar siswa dapat
 		mengembangkan kecerdasan dalam berpikir dan menemukan potensi yang tersembunyi dalam diri
