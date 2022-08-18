9c26b187f3ae63d89f63ce9f1aa50ad58c5a9a84
plty <platyunyu@gmail.com>
2022-08-18 12:49:09 +0000

    fix: unterminated literals on detailed_schedule.js

--- src/lib/data/detailed-schedule.js
+++ src/lib/data/detailed-schedule.js
@@ -1,135 +1,135 @@
 const schedule = [
-	{
-		date: "Kamis, 1 Sept 2022",
-		events: [
-			{
-				time: "12:00",
-				event: "Pembukaan Akses Soal Penyisihan\u00a01",
-			},
-		],
-	},
-	{
-		date: "Sabtu, 17 Sept 2022",
-		events: [
-			{
-				time: "23:59",
-				event: "Pengumpulan Lembar Jawaban Penyisihan/u00a01",
-			},
-		],
-	},
-	{
-		date: "Minggu, 18 Sept 2022",
-		events: [
-			{
-				time: "16:00–18:00",
-				event: "Live class Brilliant Competition XIV",
-			},
-		],
-	},
-	{
-		date: "Selasa, 20 Sept 2022",
-		events: [
-			{
-				time: "17:00",
-				event: "Pengumuman Penyisihan/u00a01",
-			},
-		],
-	},
-	{
-		date: "Rabu, 21 Sept 2022",
-		events: [
-			{
-				time: "16:00-17:00",
-				event: "Technical Meeting Penyisihan 2",
-			},
-		],
-	},
-	{
-		date: "Jumat, 23 Sept 2022",
-		events: [
-			{
-				time: "06:30-07:00",
-				event: "Link Zoom dibuka",
-			},
-			{
-				time: "07:00-08:00",
-				event: "Pembukaan",
-			},
-			{
-				time: "08:00-09:00",
-				event: "Persiapan Penyisihan 2",
-			},
-			{
-				time: "09:00-10:30",
-				event: "Penyisihan 2",
-			},
-			{
-				time: "10:30-12:00",
-				event: "Workshop Kimia",
-			},
-			{
-				time: "12:00-13:15",
-				event: "Ishoma",
-			},
-			{
-				time: "13:15-13:30",
-				event: "Pengumuman Penyisihan 2",
-			},
-			{
-				time: "13:30-14:00",
-				event: "Technical Meeting Semifinal",
-			},
-			{
-				time: "14:00-14:30",
-				event: "Persiapan Semifinal",
-			},
-			{
-				time: "14:30-16:00",
-				event: "Semifinal Math Sesi 1",
-			},
-			{
-				time: "14:30-17:00",
-				event: "Semifinal IPA",
-			},
-			{
-				time: "16:00-16:30",
-				event: "Istirahat Semifinal Math",
-			},
-			{
-				time: "16:30-18:00",
-				event: "Semifinal Math Sesi 2",
-			},
-		],
-	},
-	{
-		date: "Sabtu, 24 Sept 2022",
-		events: [
-			{
-				time: "06:30-07:00",
-				event: "Link zoom dibuka",
-			},
-			{
-				time: "07:00-07:30",
-				event: "Technical Meeting Final,
-			},
-			{
-				time: "07:30-08:00",
-				event: "Persiapan Final",
-			},
-			{
-				time: "08:00-11:30",
-				event: "Final Math dan IPA",
-			},
-			{
-				time: "11:30-12:30",
-				event: "Ishoma",
-			},
-			{
-				time: "12:30-Selesai",
-				event: "Pengumuman Final dan Acara Penutupan",
-			},
-		],
-	},
+  {
+    date: "Kamis, 1 Sept 2022",
+    events: [
+      {
+        time: "12:00",
+        event: "Pembukaan Akses Soal Penyisihan\u00a01",
+      },
+    ],
+  },
+  {
+    date: "Sabtu, 17 Sept 2022",
+    events: [
+      {
+        time: "23:59",
+        event: "Pengumpulan Lembar Jawaban Penyisihan/u00a01",
+      },
+    ],
+  },
+  {
+    date: "Minggu, 18 Sept 2022",
+    events: [
+      {
+        time: "16:00–18:00",
+        event: "Live class Brilliant Competition XIV",
+      },
+    ],
+  },
+  {
+    date: "Selasa, 20 Sept 2022",
+    events: [
+      {
+        time: "17:00",
+        event: "Pengumuman Penyisihan/u00a01",
+      },
+    ],
+  },
+  {
+    date: "Rabu, 21 Sept 2022",
+    events: [
+      {
+        time: "16:00-17:00",
+        event: "Technical Meeting Penyisihan 2",
+      },
+    ],
+  },
+  {
+    date: "Jumat, 23 Sept 2022",
+    events: [
+      {
+        time: "06:30-07:00",
+        event: "Link Zoom dibuka",
+      },
+      {
+        time: "07:00-08:00",
+        event: "Pembukaan",
+      },
+      {
+        time: "08:00-09:00",
+        event: "Persiapan Penyisihan 2",
+      },
+      {
+        time: "09:00-10:30",
+        event: "Penyisihan 2",
+      },
+      {
+        time: "10:30-12:00",
+        event: "Workshop Kimia",
+      },
+      {
+        time: "12:00-13:15",
+        event: "Ishoma",
+      },
+      {
+        time: "13:15-13:30",
+        event: "Pengumuman Penyisihan 2",
+      },
+      {
+        time: "13:30-14:00",
+        event: "Technical Meeting Semifinal",
+      },
+      {
+        time: "14:00-14:30",
+        event: "Persiapan Semifinal",
+      },
+      {
+        time: "14:30-16:00",
+        event: "Semifinal Math Sesi 1",
+      },
+      {
+        time: "14:30-17:00",
+        event: "Semifinal IPA",
+      },
+      {
+        time: "16:00-16:30",
+        event: "Istirahat Semifinal Math",
+      },
+      {
+        time: "16:30-18:00",
+        event: "Semifinal Math Sesi 2",
+      },
+    ],
+  },
+  {
+    date: "Sabtu, 24 Sept 2022",
+    events: [
+      {
+        time: "06:30-07:00",
+        event: "Link zoom dibuka",
+      },
+      {
+        time: "07:00-07:30",
+        event: "Technical Meeting Final",
+      },
+      {
+        time: "07:30-08:00",
+        event: "Persiapan Final",
+      },
+      {
+        time: "08:00-11:30",
+        event: "Final Math dan IPA",
+      },
+      {
+        time: "11:30-12:30",
+        event: "Ishoma",
+      },
+      {
+        time: "12:30-Selesai",
+        event: "Pengumuman Final dan Acara Penutupan",
+      },
+    ],
+  },
 ];
 
 export default schedule;
