5f4e481562b1e90f467fdeb742f33b2271a0ed01
plty <platyunyu@gmail.com>
2022-08-20 14:24:47 +0000

    style: change to stone

--- src/lib/component/HowToJoin/index.svelte
+++ src/lib/component/HowToJoin/index.svelte
@@ -2,8 +2,8 @@
 	import bca from "./bca.svg";
 </script>
 
-<h1 class="font-montserrat italic text-center text-5xl text-WhiteL">RULES</h1>
-<h2 class="font-montserrat font-black text-WhiteL text-center text-2x1 font-black mt-3 mb-2 pt-2">
+<h1 class="font-montserrat italic text-center text-5xl text-gray-200">RULES</h1>
+<h2 class="font-montserrat font-black text-gray-200 text-center text-2x1 font-black mt-3 mb-2 pt-2">
 	Guide Pendaftaran & Lomba
 </h2>
 <div class="flex flex-row justify-center space-x-4 ">
@@ -15,8 +15,8 @@
 	>
 		Guide Pendaftaran & Lomba
 	</a>
-</div>	
-<h2 class="font-montserrat font-black text-WhiteL text-center text-2x1 font-black mt-3 mb-2 pt-2">
+</div>
+<h2 class="font-montserrat font-black text-gray-200 text-center text-2x1 font-black mt-3 mb-2 pt-2">
 	Guide Lomba Matematika
 </h2>
 <div class="flex flex-row justify-center space-x-4 ">
@@ -30,51 +30,27 @@
 	</a>
 </div>
 
-<h2 class="font-montserrat text-center text-2xl text-WhiteL font-black mt-6">
+<h2 class="font-montserrat text-center text-2xl text-gray-200 font-black mt-6">
 	Pembayaran Lomba (Rp 125.000,00/tim)
-	
 </h2>
 <div class="flex flex-row justify-center mt-2">
-	<div class="w-72 shadow-lg rounded bg-orangeishyellow dark:bg-gray-800 p-4">
-		<div class="bg-orangeishyellow flex-row gap-4 flex justify-center items-center">
-			<div class="flex-shrink-0">
+	<div class="bg-stone-600 shadow-lg rounded px-6 py-4 drop-shadow">
+		<div class="flex-row space-x-8 flex justify-center items-center">
+			<div class="flex-shrink-0 bg-white rounded-full p-2">
 				<img alt="BCA" src={bca} class="mx-auto object-cover rounded-full h-16 w-16 " />
 			</div>
 			<div class=" flex flex-col">
-				<span class="font-barlow font-semibold text-white dark:text-white text-lg"
-					>6050 5757 97</span
-				>
-				<span class="font-barlow text-white text-xs"> Lucia Helda Sofyananigrum </span>
-				<span class="font-barlow text-white text-xs"> Dimas Ambrosius Kridhananda </span>
+				<span class="font-barlow font-semibold text-lg text-white">6050 5757 97</span>
+				<span class="font-barlow text-xs text-white"> Lucia Helda Sofyananigrum </span>
+				<span class="font-barlow text-xs text-white"> Dimas Ambrosius Kridhananda </span>
 			</div>
 		</div>
 	</div>
 </div>
 
-<!-- <h2 class="font-montserrat text-center text-2xl mt-6">Isi form pendaftaran</h2>
-<div class="flex flex-row justify-center mt-2">
-	<a target="_blank" href="/register" class="btn w-64"> Kerjakan Form </a>
-</div> -->
-<!-- <h2 class="font-montserrat text-center text-2xl mt-6">Kumpulkan Jawaban Sebelum 1 September</h2>
-<div class="flex flex-row justify-center mt-2">
-	<a
-		target="_blank"
-		rel="noreferrer"
-		href="https://docs.google.com/forms/d/e/1FAIpQLScs8F5TeQdbZImYiSKeU18wKVVTmxkW8lJ2pHPqw-_IYU85vw/viewform"
-		class="btn w-64"
-	>
-		Kumpulkan
-	</a>
-</div> -->
-
-<!-- <h2 class="font-montserrat text-center text-2xl mt-6">Tunggu Pengumuman Tanggal 3 September</h2>
-<div class="max-w-3xl mx-auto mt-2">
-	80% dari peserta dan sebanyak-banyak nya 50 tim dari setiap bidang akan lolos maju ke tahap
-	selanjutnya
-</div> -->
 <style lang="postcss">
 	.btn {
-		@apply font-barlow font-medium py-2 px-4 bg-coral hover:bg-yellow-600 focus:ring-yellow-600 focus:ring-offset-yellow-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded;
+		@apply font-barlow font-medium py-2 px-4 bg-yellow-500 hover:bg-yellow-600 focus:ring-yellow-600 focus:ring-offset-yellow-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded;
 	}
 	.thiccboi {
 		text-decoration-thickness: 15%;
--- src/lib/data/qnas.js
+++ src/lib/data/qnas.js
@@ -1,37 +1,37 @@
 const s = [
-	{
-		q: "Kapan hari terakhir pendaftaran?",
-		a: "Rabu, 17 September 2022",
-	},
-	{
-		q: "Bolehkan satu tim untuk seorang saja?",
-		a: "Boleh",
-	},
-	{
-		q: "Berapa biaya pendaftaran?",
-		a: "Early (20 Agustus - 31 Agustus 2022) : Rp 125.000,00 - per tim, Regular (1 September - 17 September 2022) : Rp 150.000,00 - per tim",
-	},
-	{
-		q: "Dimana bisa akses soal P1?",
-		a: "Daftar dan bayar dulu, submit ke google form. Baru email yang berisi file soal (untuk bidang IPA) atau link google form soal (untuk bidang mat) akan dikirimkan ke email peserta 1 yang sudah didaftarkan"
-	},
-	{
-		q: "Berapa banyak tim yang akan lolos dari penyisihan 2 ke semifinal?",
-		a: "15 tim",
-	},
-	{
-		q: "Berapa banyak tim yang akan lolos dari semifinal ke final?",
-		a: "6 tim",
-	},
-	{
-		q: "Apa format Brilliant Competition tahun ini?",
-		a:
-			"Brilliant Competition mengadakan 2 perlombaan tim, yaitu lomba matematika dan lomba IPA, dengan 1 tim terdiri akan sebanyak-banyaknya 2 orang",
-	},
-	{
-		q: "Apakah ada silabus untuk Brilliant Competition tahun ini?",
-		a: "Silabus Bcomp tahun ini berkisar materi OSN SMP",
-	},
+  {
+    q: "Kapan hari terakhir pendaftaran?",
+    a: "Rabu, 17 September 2022",
+  },
+  {
+    q: "Bolehkan satu tim untuk seorang saja?",
+    a: "Boleh",
+  },
+  {
+    q: "Berapa biaya pendaftaran?",
+    a: "Early (20 Agustus - 31 Agustus 2022) : Rp 125.000,00 - per tim, Regular (1 September - 17 September 2022) : Rp 150.000,00 - per tim",
+  },
+  {
+    q: "Dimana bisa akses soal P1?",
+    a: "Daftar dan bayar dulu, submit ke google form. Baru email yang berisi file soal (untuk bidang IPA) atau link google form soal (untuk bidang mat) akan dikirimkan ke email peserta 1 yang sudah didaftarkan"
+  },
+  {
+    q: "Berapa banyak tim yang akan lolos dari penyisihan 2 ke semifinal?",
+    a: "15 tim",
+  },
+  {
+    q: "Berapa banyak tim yang akan lolos dari semifinal ke final?",
+    a: "6 tim",
+  },
+  {
+    q: "Apa format Brilliant Competition tahun ini?",
+    a:
+      "Brilliant Competition mengadakan 2 perlombaan tim, yaitu lomba matematika dan lomba IPA, dengan 1 tim terdiri akan sebanyak-banyaknya 2 orang",
+  },
+  {
+    q: "Apakah ada silabus untuk Brilliant Competition tahun ini?",
+    a: "Silabus Bcomp tahun ini berkisar materi OSN SMP",
+  },
 ];
 
 export default s;
