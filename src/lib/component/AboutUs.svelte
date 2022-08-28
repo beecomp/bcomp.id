4f9437fece11edd802265c0666f690e390774964
Nicholas Ryan PH <nicholasryanph@gmail.com>
2022-08-28 22:22:31 +0700

    change 60 to 50 in about us

--- src/lib/component/AboutUs.svelte
+++ src/lib/component/AboutUs.svelte
@@ -5,7 +5,7 @@
 		Brilliant Competition XIV adalah kegiatan kompetisi olimpiade untuk bidang matematika dan sains
 		tingkat SMP berskala nasional yang akan diselenggarakan untuk ketiga belas kalinya secara online
 		di SMAK PENABUR Gading Serpong. Brilliant Competition XIII sebelumnya diikuti oleh ratusan
-		peserta dari sembilan provinsi dan lebih dari 60 sekolah yang tersebar di Indonesia. Brilliant
+		peserta dari sembilan provinsi dan lebih dari 50 sekolah yang tersebar di Indonesia. Brilliant
 		Competition dirancang dan dilaksanakan oleh seluruh siswa Brilliant Class di SMAK Penabur Gading
 		Serpong beserta dengan guru-guru terpilih dari Brilliant Class. Tema Brilliant Competition XIV
 		tahun ini adalah “A New Age of Brilliance”. Pada Brilliant Competition XIV, kami mengajak
