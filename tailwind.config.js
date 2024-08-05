e6541dc26f5696ab3e8fffeb4d5eecc469cfd218
Bearylover <156732418+Bearylover@users.noreply.github.com>
2024-08-05 19:13:25 +0700

    new color scheme :/

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,8 @@ module.exports = {
                 semi: ["Barlow Semi Condensed", sansFonts],
             },
             colors: {
-                'pink-2024' : '#f7ae9f',
-                'shadepink-2024' : '#de9c8f',
-                'darkpink-2024' : '#b17c72',
+                'yellow-2024' : '#ffb435',
+                'shadeyellow-2024' : '#ff9c44',
                 'blue-2024' : '#76cabd',
                 'shadeblue-2024' : '#5ea197',
                 'shadeblue2-2024' : '#528d84',
