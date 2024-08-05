2161aa5d244b26a1fcbb96bc20ffde981fb93460
Bearylover <156732418+Bearylover@users.noreply.github.com>
2024-08-05 19:22:57 +0700

    color change

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,10 +12,9 @@ module.exports = {
             colors: {
                 'yellow-2024' : '#ffb435',
                 'shadeyellow-2024' : '#ff9c44',
-                'blue-2024' : '#76cabd',
-                'shadeblue-2024' : '#5ea197',
-                'shadeblue2-2024' : '#528d84',
-                'darkblue-2024' : '#028080',
+                'blue-2024' : '#d8fcf4',
+                'shadeblue-2024' : '#0bb4a1',
+                'shadeblue2-2024' : '#089080',
                 'silver' : '#c0c0c0',
                 'bronze' : '#cf7f32',
                 'gold' : '#ffde40',
